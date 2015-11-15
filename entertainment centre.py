f39f587a7fb8cd905d8b3211101ef88079c659d5
charsi <nishil@nishils-MacBook-Pro.local>
2015-11-15 07:54:08 +0000

    initial commit

---
+++ entertainment centre.py
@@ -0,0 +1,34 @@
+import media
+import fresh_tomatoes
+
+reservoir_dogs = media.Movie(
+    "Reservoir Dogs",
+    "After a simple jewelery heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
+    "http://i.imgur.com/1cmyEND.jpg",
+    "https://www.youtube.com/watch?v=YNvLLKLwmwY",
+    1992
+    )
+
+in_bruges = media.Movie(
+    "In Bruges",
+    "Guilt-stricken after a job gone wrong, hitman Ray and his partner await orders from their ruthless boss in Bruges, Belgium, the last place in the world Ray wants to be.",
+    "http://www.impawards.com/2008/posters/in_bruges_ver2_xlg.jpg",
+    "https://www.youtube.com/watch?v=bqOJqR1cjhI",
+    2008
+    )
+
+def print_storyline(movie):
+    print(movie.title+" ee :\n"+movie.storyline)
+
+print_storyline(reservoir_dogs)
+print(in_bruges.year)
+
+##in_bruges.show_trailer()
+
+movies = [reservoir_dogs, in_bruges]
+
+##fresh_tomatoes.open_movies_page(movies)
+
+print(media.Movie.__doc__)
+print(media.Movie.__name__)
+print(media.Movie.__module__)
---
+++ media.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+class Movie():
+	"""just a class defining a movie objects"""
+
+	def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_yt_url, release_year):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster = poster_image_url
+		self.trailer = trailer_yt_url
+		self.year = release_year
+
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer)
