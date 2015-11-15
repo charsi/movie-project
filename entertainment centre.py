ba2c16cc2a19b22b1b367200aa0a3415c13a25c6
charsi <nishil@nishils-MacBook-Pro.local>
2015-11-15 11:49:17 +0000

    added imdb link

--- entertainment centre.py
+++ entertainment centre.py
@@ -6,6 +6,7 @@ reservoir_dogs = media.Movie(
     "After a simple jewelery heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
     "http://i.imgur.com/1cmyEND.jpg",
     "https://www.youtube.com/watch?v=YNvLLKLwmwY",
+    "http://www.imdb.com/title/tt0105236",
     1992
     )
 
@@ -14,6 +15,7 @@ in_bruges = media.Movie(
     "Guilt-stricken after a job gone wrong, hitman Ray and his partner await orders from their ruthless boss in Bruges, Belgium, the last place in the world Ray wants to be.",
     "http://www.impawards.com/2008/posters/in_bruges_ver2_xlg.jpg",
     "https://www.youtube.com/watch?v=bqOJqR1cjhI",
+    "http://www.imdb.com/title/tt0780536",
     2008
     )
 
@@ -22,6 +24,7 @@ kkg = media.Movie(
     "A Delhi based retired middle class man tries half-heartedly to get his land back from a swindling property dealer with the help of his sons and their friends.",
     "http://www.impawards.com/intl/india/2006/posters/khosla_ka_ghosla.jpg",
     "https://www.youtube.com/watch?v=jUy9ho06RuQ",
+    "http://www.imdb.com/title/tt0466460",
     2006
     )
 
@@ -30,6 +33,7 @@ gow2 = media.Movie(
     " A clash between Sultan (a Qureishi dacoit chief) and Shahid Khan (a Pathan who impersonates him) leads to the expulsion of Khan from Wasseypur, and ignites a deadly blood feud spanning three generations.",
     "http://www.paragsankhe.com/wp-content/uploads/2013/01/Gangs-of-Wasseypur_UK_30x40_Coal_Poster.jpg",
     "https://www.youtube.com/watch?v=Z1nmIm5ncNE",
+    "http://www.imdb.com/title/tt1954470",
     2012
     )
 
@@ -38,21 +42,36 @@ monsoon_wedding = media.Movie(
     "A stressed father, a bride-to-be with a secret, a smitten event planner, and relatives from around the world create much ado about the preparations for an arranged marriage in India.",
     "http://www.amoeba.com/admin/uploads/blog/Eric_B/monsoon_wedding_ver6_xlg.jpg",
     "https://www.youtube.com/watch?v=eaP-UrmS6Ww",
+    "http://www.imdb.com/title/tt0265343",
     2001
     )
 
+true_romance = media.Movie(
+    "True Romance",
+    "Clarence marries hooker Alabama, steals cocaine from her pimp, and tries to sell it in Hollywood, while the owners of the coke try to reclaim it.",
+    "http://cdn-images.9cloud.us/22/piccit_true_romance_1993_1534_x_217_2069657734.jpg",
+    "https://www.youtube.com/watch?v=_wNYNDzKpuQ",
+    "http://www.imdb.com/title/tt0108399/",
+    1993
+    )
+
+ollo = media.Movie(
+    "Oye Lucky Lucky Oye",
+    " A burglar reflects on his life and loves; while the media speculates how he managed to pull off so many heists.",
+    "http://www.impawards.com/intl/india/2008/posters/oye_lucky_lucky_oye.jpg",
+    "https://www.youtube.com/watch?v=3paPF30NJhg",
+    "http://www.imdb.com/title/tt1292703/",
+    1993
+    )
+
 def print_storyline(movie):
-    print(movie.title+" ee :\n"+movie.storyline)
+    print(movie.title+"\n"+movie.storyline)
 
-print_storyline(reservoir_dogs)
-print(in_bruges.year)
+#print_storyline(reservoir_dogs)
+#print(in_bruges.year)
 
-##in_bruges.show_trailer()
+#Movies to be included on the page
+movies = [reservoir_dogs, in_bruges, kkg, gow2, monsoon_wedding, true_romance, ollo]
 
-movies = [reservoir_dogs, in_bruges, kkg, gow2, monsoon_wedding]
-
+#create HTML page
 fresh_tomatoes.open_movies_page(movies)
-
-##print(media.Movie.__doc__)
-##print(media.Movie.__name__)
-##print(media.Movie.__module__)
